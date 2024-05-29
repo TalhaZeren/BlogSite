4808dfa64c945ff72004a416d092f281ed7302ee
foreign <foreign@gmail.com>
2024-05-29 23:30:04 +0300

    Like sinifi olusturuldu.

---
+++ src/main/java/com/blogYazilimApp/myBlogSiteApp/Entities/Like.java
@@ -0,0 +1,31 @@
+package com.blogYazilimApp.myBlogSiteApp.Entities;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import org.springframework.data.annotation.Id;
+
+@Entity
+@Table(name = "like")
+@Data
+public class Like {
+
+    @Id
+    Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id",nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id",nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    Post post;
+
+}
